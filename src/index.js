f99a91a946ce25aaab3228347bc70c26b418e773
Revanth Kumar Annavarapu <35203638+revanth0212@users.noreply.github.com>
2019-12-18 10:23:39 -0600

    Added tapable package

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import { SyncHook } from "tapable";
+
 import ReactDOM from "react-dom";
+
+const counterHook = new SyncHook();
+
+counterHook.tap("Outside", count => {
+  console.log("Outside", count);
+});
 
 const useCounter = () => {
   const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    counterHook.tap("Inside", count => {
+      console.log("Inside", count);
+    });
+  }, []);
+
   const addCount = useCallback(() => {
     setCounter(counter + 1);
+    counterHook.call(counter + 1);
   }, [counter, setCounter]);
+
   const reduceCount = useCallback(() => {
     setCounter(counter - 1);
+    counterHook.call(counter - 1);
   }, [counter, setCounter]);
 
   return [counter, { addCount, reduceCount }];
